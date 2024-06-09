cc38d00369169d64247044ad2ad79b6c895275e5
ZuYun <jonas.jzy@gmail.com>
2024-06-10 03:03:20 +0800

    🎉

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # auto-service
+
+![](https://img.shields.io/badge/AutoService-1.1.1-brightgreen.svg)
+
+[![License](https://img.shields.io/badge/LICENSE-Apache%202-green.svg?style=flat-square)](https://www.apache.org/licenses/LICENSE-2.0)
+
 generate META-INF/services/xxxxx  for 【AutoService】
---
+++ auto-service/build.gradle.kts
@@ -0,0 +1,78 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import wing.publishJava5hmlA
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(wings.plugins.android)
+//    kotlin("jvm") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.28.0"
+}
+
+kotlin {
+    // Or shorter:
+    jvmToolchain(18)
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_18)
+        freeCompilerArgs.add("-Xcontext-receivers")
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+    }
+}
+
+//https://kotlinlang.org/docs/ksp-incremental.html#aggregating-vs-isolating
+dependencies {
+//    implementation("com.squareup:kotlinpoet-ksp:1.16.0")
+    implementation(libs.ksp.process.api)
+    // https://mvnrepository.com/artifact/com.google.auto.service/auto-service
+    implementation(libs.google.auto.service.anno)
+
+}
+
+group = "io.github.5hmla"
+version = "0.0.1"
+
+publishJava5hmlA("ksp library for Google AutoService 🚀")
+
+mavenPublishing {
+    coordinates("io.github.5hmla", "auto-service", "0.0.1")
+}
+//KSFile
+//  packageName: KSName
+//  fileName: String
+//  annotations: List<KSAnnotation>  (File annotations)
+//  declarations: List<KSDeclaration>
+//    KSClassDeclaration // class, interface, object
+//      simpleName: KSName
+//      qualifiedName: KSName
+//      containingFile: String
+//      typeParameters: KSTypeParameter
+//      parentDeclaration: KSDeclaration
+//      classKind: ClassKind
+//      primaryConstructor: KSFunctionDeclaration
+//      superTypes: List<KSTypeReference>
+//      // contains inner classes, member functions, properties, etc.
+//      declarations: List<KSDeclaration>
+//    KSFunctionDeclaration // top level function
+//      simpleName: KSName
+//      qualifiedName: KSName
+//      containingFile: String
+//      typeParameters: KSTypeParameter
+//      parentDeclaration: KSDeclaration
+//      functionKind: FunctionKind
+//      extensionReceiver: KSTypeReference?
+//      returnType: KSTypeReference
+//      parameters: List<KSValueParameter>
+//      // contains local classes, local functions, local variables, etc.
+//      declarations: List<KSDeclaration>
+//    KSPropertyDeclaration // global variable
+//      simpleName: KSName
+//      qualifiedName: KSName
+//      containingFile: String
+//      typeParameters: KSTypeParameter
+//      parentDeclaration: KSDeclaration
+//      extensionReceiver: KSTypeReference?
+//      type: KSTypeReference
+//      getter: KSPropertyGetter
+//        returnType: KSTypeReference
+//      setter: KSPropertySetter
+//        parameter: KSValueParameter
